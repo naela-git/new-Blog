e54be7dc5c58ba8a7f41f2a3ed2d152c954f1ac5
naela-git <naelaeng83@gmail.com>
2022-05-31 07:14:12 -0700

    test

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-# POST CONTROLLERS ##########################
+
 Route::get('/post',[App\Http\Controllers\PostController::class, 'index'])->name('post.index');
 
 
